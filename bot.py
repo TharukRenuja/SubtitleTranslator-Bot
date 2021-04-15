62514f71628221ffce598163089828e350b778c9
♰ ร ℘ ค ฬ ή ♰ <78096979+abrock00@users.noreply.github.com>
2021-04-16 03:56:45 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -72,7 +72,7 @@ def abouts(client, message):
         text=about,
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Give Feedback", url="t.me/ab_nero")]]
+            [[InlineKeyboardButton("Give Feedback", url="t.me/ab_wolf")]]
         ),
     )
 
@@ -127,7 +127,7 @@ def data(client, callback_query):
             text=about,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
-                [[InlineKeyboardButton("📬 Give Feedback 📬", url="t.me/ab_nero")]]
+                [[InlineKeyboardButton("📬 Give Feedback 📬", url="t.me/ab_wolf")]]
             ),
         )
     elif rslt == "close":
