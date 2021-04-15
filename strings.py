de2333bb9d8a751133802b9cf1d4928091bd41bb
♰ ร ℘ ค ฬ ή ♰ <78096979+abrock00@users.noreply.github.com>
2021-04-16 03:47:46 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,7 +1,10 @@
 from pyrogram.types import InlineKeyboardButton
 from creds import cred
 
-welcome = " 💫 Give me the subtitle file you want to translate.💫\n🌹 Join & ❤️ support @PF_HW\n 💐 Support Group @Hcf_binge"
+welcome = "
+🔥 Give me the subtitle file you want to translate.\n
+🏵️ Join & ❤️ support @PF_HW\n
+ 🏵️ Support Group @Hcf_binge"
 about = (
     "`CHANNEL` : [HOLLYWOOD MOVIES](t.me/PF_HW)\n`Creator :` [ ፕ๏χɪϲ ɯ๏ʅϝ ](t.me/Ab_wolf)\n`Language:` ["
     "Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku]("
